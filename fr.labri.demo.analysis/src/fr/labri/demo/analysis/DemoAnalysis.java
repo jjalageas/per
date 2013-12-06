0a526ef4f24550be8e8ed8bedf358a84042d0369
jjalageas <jjalageas@yahoo.com>
2013-12-06 22:36:40 +0100

    changed link detection algorithm

--- fr.labri.demo.analysis/src/fr/labri/demo/analysis/DemoAnalysis.java
+++ fr.labri.demo.analysis/src/fr/labri/demo/analysis/DemoAnalysis.java
@@ -35,61 +35,44 @@ public class DemoAnalysis extends AbstractAnalysis{
 		
 		ArrayList<String> bugReport = bugzillaReportExtractor("https://issues.apache.org/bugzilla/", "jjalageas@yahoo.com", "pepsi718");
 		ArrayList<String> links = new ArrayList<String>();
-
-		//Liste les classes contenues dans le dépôt
-		for(Object o: src.getItems().toArray())
-			System.out.println(o.toString());
 		
-		//Affiche le nom du workspace
-		System.out.println(src.getWorkspace().toString());
-			
 		for (Author auth : src.getAuthors()) {
-			
-			//Liste le nombre de commits par auteurs
-			HarmonyLogger.info(auth.getName()+" made "+auth.getEvents().size()+" commits to the projects: "+src.getUrl());
 			
 			for (int i=0; i<auth.getEvents().size(); i++){
 				
-				//Affiche le commit ID
-				System.out.println("Commit ID");
-				System.out.println(auth.getEvents().get(i).getNativeId());
-				
-				//Affiche la date et l'heure du commit
-				System.out.println("Commit Timestamp");
-				System.out.println(auth.getEvents().get(i).getTimestampAsString());
-				
-				//Affiche le message du commit
-				System.out.println("Commit Log");
 				String commitLog = auth.getEvents().get(i).getMetadata().get("commit_message");
-				System.out.println(commitLog);
 				
-				//Recherche d'un lien avec un bug
-				String link = compareLogToReport(commitLog, bugReport);
-				if(link != null){
-					String linkDisplay = "Commit " + auth.getEvents().get(i).getNativeId() + " linked to bug " + link;
-					links.add(linkDisplay);
+				//link search
+				ArrayList<String> link = compareLogToReport(commitLog, bugReport);
+				if(link.size() > 0){
+					for(String l: link){
+						String linkDisplay = "Commit " + auth.getEvents().get(i).getNativeId() + " linked to bug " + l;
+						links.add(linkDisplay);
 					}			
 				}
 
 			}
+		}
 		
 		for(String s: links)
 			System.out.println(s);
+		System.out.println();
 		
-		}
+	}
 	
 	
-	public String compareLogToReport(String commitLog, ArrayList<String> bugReportIds){
+	public ArrayList<String> compareLogToReport(String commitLog, ArrayList<String> bugReportIds){
 		
-		String delims = "[ , #]+";
+		String delims = "[ , #]+";	
 		String[] logTokens = commitLog.split(delims);
-
+		ArrayList<String> linkReport = new ArrayList<String>();
+		
 		for(String log: logTokens)
 			for(String report: bugReportIds)
 				if(log.equals(report))
-					return report;
-		
-		return null;
+					linkReport.add(report);
+	
+		return linkReport;
 	}
 	
 	
